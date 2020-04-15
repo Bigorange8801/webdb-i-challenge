817fa6d1314d0476a5a635146d1d54ec7c513670
John <cjd8801@gmail.com>
2020-04-15 10:56:57 -0500

    updated routes

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -1,74 +1,63 @@
-const express = require('express');
-
+const express = require('express')
 const db = require('../data/dbConfig');
-
-
-
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const accounts = await db('accounts');
-        res.json(accounts);
-    } catch (err) {
-        res.status(500).json({ message: 'Failed to retrieve accounts' });
-    }
-});
 
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const accounts = await db('accounts').where({ id });
-
-        res.json(accounts);
-    } catch (err) {
-        res.status(500).json({ message: 'Failed to retrieve accounts' });
-    }
-});
-
-router.post('/', async (req, res) => {
-    try {
-        const accountData = req.body;
-        const [id] = await db('accounts').insert(accountData);
-        const newAccountEntry = await db('accounts').where({ id });
-
-        res.status(201).json(newAccountEntry);
-    } catch (err) {
-        console.log('POST error', err);
-        res.status(500).json({ message: "Failed to store data" });
-    }
-});
-
-router.put('/:id', async (req, res) => {
-    try {
-        const editedAcctData = req.body;
-        const editedAcctDataEntry = await db('accounts')
-            .where({ id: req.params.id })
-            .update(req.body)
-            .then(count => {
-                if (count) {
-                    res.status(200).json({ message: `${count} record(s) updated` });
-                } else {
-                    res.status(404).json({ message: 'Account not found' });
-                }
-            })
-    } catch (err) {
-        res.status(500).json({ message: 'Could not update the account' });
-    }
-
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        db('accounts')
-            .where({ id: req.params.id })
-            .del()
-            .then(count => {
-                res.status(200).json({ message: `${count} record(s) deleted` });
-            })
-    } catch{
-        res.status(500).json({ message: 'Could not remove the account' });
-    }
-});
-
-module.exports = router;
+//GET /api/accounts
+router.get("/", (req, res) => {
+    db.select('*')
+      .from('accounts')
+        .then(accountInfo => {
+          res.status(200).json(accountInfo)
+        })
+        .catch(error => {
+          res.status(500).json(error)
+        })
+  })
+  
+  //POST /api/accounts
+  router.post("/", (req, res) => {
+    const postData = req.body;
+    db('accounts')
+      .insert(postData, 'id')
+        .then(([id]) => {
+            db('accounts')
+              .where({ id }) 
+              .first() 
+              .then(account => {
+                  res.status(200).json(account);
+              });
+        })
+    .catch(err => {
+        res.json(err);
+    });
+  });
+  
+  //UPDATE /api/accounts/:id
+  router.put("/:id", (req, res) => {
+    const changes = req.body
+    db('accounts')
+      .where("id", req.params.id)
+      .update(changes)
+      .then(account => {
+        res.status(200).json({ message: `Updated ${account} entries` })
+      })
+      .catch(error => {
+        res.json(error)
+      })
+  })
+  
+  //DELETE /api/accounts/:id
+  router.delete("/:id", (req, res) => {
+    db("accounts")
+      .where("id", req.params.id)
+      .del()
+      .then(account => {
+        res.status(200).json({ message: `deleted ${account} entries`})
+      })
+      .catch(error => {
+        res.json(error);
+      })
+  })
+  
+  module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const accounts = require('./accounts/accounts');
+
 const server = express();
 
+
 server.use(express.json());
+
+
+server.use("/api/accounts", accounts);
+
 
 module.exports = server;
